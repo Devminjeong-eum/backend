b280c10f838157de58fad56866a8c7a10b0943d1
RookieAND <gwangin1999@naver.com>
2024-04-23 17:03:09 +0900

    feat: Swagger Module 생성 및 bootstrap 함수에 적용

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 
+import { setupSwaggerModule } from '#/configs/swagger.config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	setupSwaggerModule(app);
+
 	await app.listen(8080);
 }
 bootstrap();
