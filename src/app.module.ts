a12c90ae639de5109f0886e12077e90bd9ce30bf
RookieAND <gwangin1999@naver.com>
2024-04-23 23:12:17 +0900

    fix: 배포 Script 내 문법 오류 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,8 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { WordModule } from './word/word.module';
 
 @Module({
-	imports: [UserModule, WordModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
--- src/word/dto/update-word.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-
-import { CreateWordDto } from './create-word.dto';
-
-export class UpdateWordDto extends PartialType(CreateWordDto) {}
