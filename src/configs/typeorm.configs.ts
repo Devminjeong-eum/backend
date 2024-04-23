5dc01fe481e65302e6a3eedd6cc55f5e967c89c8
RookieAND <gwangin1999@naver.com>
2024-04-24 00:39:38 +0900

    feat: GlobalTypeOrmModule 1차 제작

--- src/configs/typeorm.configs.ts
+++ src/configs/typeorm.configs.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 
 @Injectable()
-export class TypeormConfig implements TypeOrmOptionsFactory {
+export class TypeOrmConfig implements TypeOrmOptionsFactory {
+	constructor(private readonly configService: ConfigService) {}
 	createTypeOrmOptions(): TypeOrmModuleOptions {
 		return {
 			type: 'postgres',
-			url: '',
-			host: 'localhost',
-			port: 5432,
-			username: '',
-			password: '',
-			database: '',
+			host: this.configService.get('DB_HOST'),
+			port: this.configService.get('DB_PORT'),
+			username: this.configService.get('DB_USERNAME'),
+			password: this.configService.get('DB_PASSWORD'),
+			database: this.configService.get('DB_DATABASE'),
 			synchronize: true,
 			dropSchema: false,
 			logging: true,
@@ -19,3 +20,7 @@ export class TypeormConfig implements TypeOrmOptionsFactory {
 		};
 	}
 }
+
+export const GlobalTypeOrmModule = TypeOrmModule.forRootAsync({
+	useClass: TypeOrmConfig,
+})
