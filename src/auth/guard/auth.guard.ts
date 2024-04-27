8095b40076840af823f96f1cd2f7d65e4384553d
RookieAND <gwangin1999@naver.com>
2024-04-27 23:09:10 +0900

    feat: JWT 를 파싱하여 유저 정보를 반환하는 Guard 추가

---
+++ src/auth/guard/auth.guard.ts
@@ -0,0 +1,106 @@
+import {
+	CanActivate,
+	ExecutionContext,
+	UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+import type { Request, Response } from 'express';
+
+import { AuthService } from '../auth.service';
+import { JwtPayload } from '../interface/jwt-auth.interface';
+
+import { UserRepository } from '#/databases/repositories/user.repository';
+
+export class AuthenticationGuard implements CanActivate {
+	constructor(
+		private readonly jwtService: JwtService,
+		private readonly authService: AuthService,
+		private readonly userRepository: UserRepository,
+	) {}
+
+	async canActivate(context: ExecutionContext) {
+		const request = context.switchToHttp().getRequest<Request>();
+		const response = context.switchToHttp().getResponse<Response>();
+		const { accessToken, refreshToken } = request.cookies;
+
+		if (!accessToken || !refreshToken) {
+			throw new UnauthorizedException(
+				'요청에 계정 정보가 존재하지 않습니다.',
+			);
+		}
+
+		let userId: string;
+
+		try {
+			userId = await this.jwtService
+				.verifyAsync<JwtPayload>(accessToken)
+				.then((payload) => payload.id);
+		} catch (error) {
+			userId = await this.jwtService
+				.verifyAsync<JwtPayload>(refreshToken)
+				.then((payload) => payload.id)
+				.catch(() => {
+					this.removeAuthenticateCookie(response);
+					throw new UnauthorizedException(
+						'토큰이 만료되었습니다. 다시 로그인해주세요.',
+					);
+				});
+
+			const {
+				accessToken: reIssueAccessToken,
+				refreshToken: reIssueRefreshToken,
+			} = this.authService.getAuthenticateToken(userId);
+
+			this.setAuthenticateCookie(
+				response,
+				reIssueAccessToken,
+				reIssueRefreshToken,
+			);
+		}
+
+		const user = await this.userRepository.findById(userId);
+
+		if (!user) {
+			this.removeAuthenticateCookie(response);
+			throw new UnauthorizedException('유효하지 않은 계정 정보입니다.');
+		}
+
+		request.user = user;
+		return true;
+	}
+
+	private setAuthenticateCookie(
+		response: Response,
+		accessToken: string,
+		refreshToken: string,
+	) {
+		response.cookie('accessToken', accessToken, {
+			secure: true,
+			maxAge: 5 * 60 * 1000,
+			sameSite: 'none',
+			path: '/',
+		});
+		response.cookie('refreshToken', refreshToken, {
+			secure: true,
+			maxAge: 7 * 24 * 60 * 60 * 1000,
+			sameSite: 'none',
+			path: '/',
+		});
+	}
+
+	private removeAuthenticateCookie(response: Response) {
+		response.cookie('accessToken', '', {
+			secure: true,
+			maxAge: 0,
+			sameSite: 'none',
+			path: '/',
+		});
+		response.cookie('refreshToken', '', {
+			secure: true,
+			maxAge: 0,
+			sameSite: 'none',
+			path: '/',
+		});
+	}
+}
