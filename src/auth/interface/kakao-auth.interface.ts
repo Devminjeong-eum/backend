abba75491a2f5c3a71656c04df5d93f89e9df9f5
RookieAND <gwangin1999@naver.com>
2024-04-27 22:56:25 +0900

    feat: kakao-auth Strategy 수정 및 interface 정의

---
+++ src/auth/interface/kakao-auth.interface.ts
@@ -0,0 +1,5 @@
+export interface KakaoAuthUser {
+    email: string;
+    nickname: string;
+    profileImage: string;
+}
--- src/auth/strategies/kakao-auth.strategy.ts
+++ src/auth/strategies/kakao-auth.strategy.ts
@@ -4,18 +4,14 @@ import { PassportStrategy } from '@nestjs/passport';
 
 import { Profile, Strategy } from 'passport-kakao';
 
-import { KakaoAuthUser } from '#/auth/decorators/kakao-auth.decorator';
-
 @Injectable()
 export class KakaoStrategy extends PassportStrategy(Strategy, 'kakao') {
-	constructor(
-		private readonly configService: ConfigService,
-	) {
+	constructor(private readonly configService: ConfigService) {
 		super({
 			clientID: configService.get<string>('KAKAO_CLIENT_ID'),
 			clientSecret: configService.get<string>('KAKAO_SECRET_KEY'),
-			scope: ['profile_nickname', 'profile_image'],
 			callbackURL: '/auth/kakao/callback',
+			scope: ['profile_nickname', 'profile_image'],
 		});
 	}
 
@@ -23,15 +19,15 @@ export class KakaoStrategy extends PassportStrategy(Strategy, 'kakao') {
 		accessToken: string,
 		refreshToken: string,
 		profile: Profile,
-		done: (error: null | Error, user?: KakaoAuthUser) => void,
 	) {
+		console.log(profile._json);
 		const { email, profile_nickname, profile_image } =
 			profile._json.kakao_account;
 
-		done(null, {
+		return {
 			email,
 			nickname: profile_nickname,
 			profileImage: profile_image,
-		});
+		};
 	}
 }
