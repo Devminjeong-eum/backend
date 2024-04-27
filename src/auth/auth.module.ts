6a436799547207eb713db251223ff18d1b1cea44
RookieAND <gwangin1999@naver.com>
2024-04-27 23:09:45 +0900

    feat: AuthModule 내 의존성 모듈 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { JsonWebTokenModule } from '#/configs/jwt.config';
+import { User } from '#/databases/entities/user.entity';
+import { UserRepository } from '#/databases/repositories/user.repository';
+import { UserModule } from '#/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { KakaoStrategy } from './strategies/kakao-auth.strategy';
 
 @Module({
-	imports: [PassportModule, JsonWebTokenModule],
+	imports: [
+		TypeOrmModule.forFeature([User]),
+		PassportModule,
+		JsonWebTokenModule,
+		UserModule,
+	],
 	controllers: [AuthController],
-	providers: [AuthService, KakaoStrategy],
+	providers: [AuthService, KakaoStrategy, UserRepository],
 })
 export class AuthModule {}
