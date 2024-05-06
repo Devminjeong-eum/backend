bd5b7880ae2943320105a2f77c0799d8d2277846
RookieAND <gwangin1999@naver.com>
2024-05-06 17:28:50 +0900

    feat: SpreadSheet 에서 데이터를 가져오는 로직 수정

--- src/spread-sheet/spread-sheet.service.ts
+++ src/spread-sheet/spread-sheet.service.ts
@@ -9,36 +9,54 @@ import { SpreadConnectService } from './spread-connect.service';
 @Injectable()
 export class SpreadSheetService {
 	private readonly jwtClient: JWT;
-	private loadedSpreadSheet: sheets_v4.Schema$Spreadsheet
+	private readonly spreadSheetId: string;
 
 	constructor(
 		private readonly configService: ConfigService,
 		private readonly spreadConnectService: SpreadConnectService,
 	) {
+		const spreadSheetId = this.configService.get<string>(
+			'GOOGLE_SPREAD_SHEET_ID',
+		);
+
+		if (!spreadSheetId) {
+			throw new InternalServerErrorException(
+				'GOOGLE_SPREAD_SHEET_ID 가 누락되었습니다.',
+			);
+		}
+
 		this.jwtClient = this.spreadConnectService.getGoogleAuthorize();
+		this.spreadSheetId = spreadSheetId;
 	}
 
 	private getGoogleSheetConnect(): sheets_v4.Sheets {
 		return google.sheets({ version: 'v4', auth: this.jwtClient });
 	}
 
-	async loadSpreadSheet() {
-		const spreadSheetId = this.configService.get<string>(
-			'GOOGLE_SPREAD_SHEET_ID',
-		);
-
-		if (!spreadSheetId)
-			throw new InternalServerErrorException(
-				'GOOGLE_SPREAD_SHEET_ID 가 누락되었습니다.',
-			);
-
-		const googleSheet = this.getGoogleSheetConnect();
-		const spreadSheet = await googleSheet.spreadsheets.get({
-			spreadsheetId: spreadSheetId,
-			includeGridData: true,
+	async getRangeCellData() {
+		const sheets = this.getGoogleSheetConnect();
+		const rangeCells = await sheets.spreadsheets.values.get({
+			spreadsheetId: this.spreadSheetId,
+			range: `${process.env.NODE_ENV}!A2:Z`,
 		});
 
-		this.loadedSpreadSheet = spreadSheet.data;
-		return spreadSheet.data;
+		const rangeCellValues: string[][] =
+			rangeCells.data.values ?? [];
+
+		return rangeCellValues;
+	}
+
+	async insertCellData(range: string, value: string) {
+		const sheets = this.getGoogleSheetConnect();
+		const response = await sheets.spreadsheets.values.update({
+			spreadsheetId: this.spreadSheetId,
+			range: `${process.env.NODE_ENV}!${range}`,
+			valueInputOption: 'RAW',
+			requestBody: {
+				values: [[value]],
+			},
+		});
+
+		return response.data.updatedCells ?? 0;
 	}
 }
