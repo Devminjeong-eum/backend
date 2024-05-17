dcdb4a5d81d0a62bca7352d5e11ae657e9e0101f
Baik Gwangin <gwangin1999@naver.com>
2024-05-18 00:42:36 +0900

    feat: LikeModule 생성 및 AppModule 에 DI 적용

---
+++ src/like/like.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Like } from '#databases/entities/like.entity';
+import { User } from '#databases/entities/user.entity';
+import { Word } from '#databases/entities/word.entity';
+import { LikeRepository } from '#databases/repositories/like.repository';
+import { UserRepository } from '#databases/repositories/user.repository';
+import { WordRepository } from '#databases/repositories/word.repository';
+
+import { LikeController } from './like.controller';
+import { LikeService } from './like.service';
+
+@Module({
+	imports: [TypeOrmModule.forFeature([Like, User, Word])],
+	controllers: [LikeController],
+	providers: [
+		// Service
+		LikeService,
+		// Repository
+		LikeRepository,
+		UserRepository,
+		WordRepository,
+	],
+})
+export class LikeModule {}
