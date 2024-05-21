e408af80befacd7af923f8439bcdfd29a50a3215
RookieAND <gwangin1999@naver.com>
2024-05-22 02:07:12 +0900

    feat: UserInformationInterceptor 에 TEST ADMIN 로직 추가

--- src/user/interceptors/user-information.interceptor.ts
+++ src/user/interceptors/user-information.interceptor.ts
@@ -2,8 +2,10 @@ import {
 	CallHandler,
 	ExecutionContext,
 	Injectable,
+	InternalServerErrorException,
 	NestInterceptor,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 import { UserRepository } from '#databases/repositories/user.repository';
 
@@ -13,11 +15,28 @@ import { AuthService } from '../../auth/auth.service';
 export class UserInformationInterceptor implements NestInterceptor {
 	constructor(
 		private readonly authService: AuthService,
+		private readonly configService: ConfigService,
 		private readonly userRepository: UserRepository,
 	) {}
 
 	async intercept(context: ExecutionContext, next: CallHandler) {
 		const request = context.switchToHttp().getRequest();
+
+		// NOTE : 원활한 개발을 위해 임시로 생성한 Admin 계정에 접근 가능하도록 하는 Key
+		const { admin_key: requestAdminKey } = request.headers;
+		const adminKey = this.configService.get<string>('TEST_ADMIN_KEY');
+
+		if (requestAdminKey && adminKey && requestAdminKey === adminKey) {
+			const adminUser = await this.userRepository.findById(adminKey);
+
+			if (!adminUser)
+				throw new InternalServerErrorException(
+					'Admin User 정보가 DB 에 존재하지 않습니다.',
+				);
+
+			request.user = adminUser;
+			return next.handle();
+		}
 
 		const { accessToken, refreshToken } = request.cookies ?? {};
 
