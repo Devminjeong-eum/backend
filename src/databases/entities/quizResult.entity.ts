6411cf6666ef007b015e640106c706988b553dbf
RookieAND <gwangin1999@naver.com>
2024-05-24 00:29:42 +0900

    fix: Word Table 과 ManyToOne 관계를 맺지 않고, ID 만 저장하도록 수정

--- src/databases/entities/quizResult.entity.ts
+++ src/databases/entities/quizResult.entity.ts
@@ -1,7 +1,6 @@
-import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 import { User } from '#databases/entities/user.entity';
-import { Word } from '#databases/entities/word.entity';
 
 @Entity()
 export class QuizResult {
@@ -12,11 +11,9 @@ export class QuizResult {
 	@JoinColumn({ name: 'userId' })
 	user: User;
 
-	@ManyToOne(() => Word)
-	@JoinColumn({ name: 'correctWordIds' })
-	correctWordIds: Word[];
-
-	@ManyToOne(() => Word)
-	@JoinColumn({ name: 'incorrectWordIds' })
+	@Column('uuid', { array: true })
+	correctWordIds: string[];
+  
+	@Column('uuid', { array: true })
 	incorrectWordIds: string[];
 }
