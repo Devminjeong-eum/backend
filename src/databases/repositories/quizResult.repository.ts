ac24d269f8334a01aa801c59885a81e6885eb164
RookieAND <gwangin1999@naver.com>
2024-05-24 00:30:23 +0900

    feat: Quiz 결과를 생성하는 Repository Method 생성

--- src/databases/repositories/quizResult.repository.ts
+++ src/databases/repositories/quizResult.repository.ts
@@ -3,7 +3,9 @@ import { InjectRepository } from '@nestjs/typeorm';
 
 import { Repository } from 'typeorm';
 
-import { QuizResult } from '#/databases/entities/quizResult.entity';
+import { RequestCreateQuizResultDto } from '#/quiz/dto/create-quiz-result.dto';
+import { QuizResult } from '#databases/entities/quizResult.entity';
+import { User } from '#databases/entities/user.entity';
 
 @Injectable()
 export class QuizResultRepository {
@@ -11,4 +13,19 @@ export class QuizResultRepository {
 		@InjectRepository(QuizResult)
 		private readonly quizResultRepository: Repository<QuizResult>,
 	) {}
+
+	async create(createQuizResultDto: RequestCreateQuizResultDto) {
+		const { userId, correctWordIds, incorrectWordIds } =
+			createQuizResultDto;
+
+		const user = new User();
+		user.id = userId;
+
+		const quizResult = new QuizResult();
+		quizResult.user = user;
+		quizResult.correctWordIds = correctWordIds;
+		quizResult.incorrectWordIds = incorrectWordIds;
+
+		return this.quizResultRepository.save(quizResult);
+	}
 }
