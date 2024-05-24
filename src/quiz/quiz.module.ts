baf2ac8acfd083d1515afa5580f8dc8f0180144b
RookieAND <gwangin1999@naver.com>
2024-05-25 05:51:48 +0900

    feat: QuizModule 에 QuizSelection 관련 DI 적용

--- src/quiz/quiz.module.ts
+++ src/quiz/quiz.module.ts
@@ -6,16 +6,19 @@ import { UserModule } from '#/user/user.module';
 import { WordModule } from '#/word/word.module';
 import { QuizResult } from '#databases/entities/quizResult.entity';
 import { QuizResultRepository } from '#databases/repositories/quizResult.repository';
+import { QuizSelectionRepository } from '#databases/repositories/quizSelection.repository';
 
 import { QuizController } from './quiz.controller';
 import { QuizService } from './quiz.service';
+import { SpreadSheetModule } from '#/spread-sheet/spread-sheet.module';
 
 @Module({
 	imports: [
-		TypeOrmModule.forFeature([QuizResult]),
+		TypeOrmModule.forFeature([QuizResult, QuizSelectionRepository]),
 		AuthModule,
 		UserModule,
 		WordModule,
+		SpreadSheetModule,
 	],
 	controllers: [QuizController],
 	providers: [
@@ -23,6 +26,7 @@ import { QuizService } from './quiz.service';
 		QuizService,
 		// Repository
 		QuizResultRepository,
+		QuizSelectionRepository,
 	],
 })
 export class QuizModule {}
