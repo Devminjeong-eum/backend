19f1cfff1ae6a58553482f8455bb034851c8335e
RookieAND <gwangin1999@naver.com>
2024-05-25 20:05:24 +0900

    fix: QuizSelection DB 를 QuizModule 에 DI 하는 방식 적용

--- src/quiz/quiz.module.ts
+++ src/quiz/quiz.module.ts
@@ -2,19 +2,20 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthModule } from '#/auth/auth.module';
+import { SpreadSheetModule } from '#/spread-sheet/spread-sheet.module';
 import { UserModule } from '#/user/user.module';
 import { WordModule } from '#/word/word.module';
 import { QuizResult } from '#databases/entities/quizResult.entity';
+import { QuizSelection } from '#databases/entities/quizSelection.entity';
 import { QuizResultRepository } from '#databases/repositories/quizResult.repository';
 import { QuizSelectionRepository } from '#databases/repositories/quizSelection.repository';
 
 import { QuizController } from './quiz.controller';
 import { QuizService } from './quiz.service';
-import { SpreadSheetModule } from '#/spread-sheet/spread-sheet.module';
 
 @Module({
 	imports: [
-		TypeOrmModule.forFeature([QuizResult, QuizSelectionRepository]),
+		TypeOrmModule.forFeature([QuizResult, QuizSelection]),
 		AuthModule,
 		UserModule,
 		WordModule,
