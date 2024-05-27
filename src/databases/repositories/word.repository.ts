3e7ffb4c6d65e29da182bbd62a27c1ee3632905e
RookieAND <gwangin1999@naver.com>
2024-05-28 01:41:20 +0900

    feat: 단어 정렬 기준에 따라 orderBy 에 다른 값을 주입하도록 수정

--- src/databases/repositories/word.repository.ts
+++ src/databases/repositories/word.repository.ts
@@ -23,6 +23,7 @@ import {
 	RequestWordUserLikeDto,
 	ResponseWordUserLikeDto,
 } from '#/word/dto/word-user-like.dto';
+import { WORD_SORTING_TYPE } from '#/word/interface/word-list-sorting.interface';
 import { Word } from '#databases/entities/word.entity';
 
 @Injectable()
@@ -169,33 +170,44 @@ export class WordRepository {
 	}
 
 	async findWithList(requestWordListDto: RequestWordListDto) {
-		const { userId } = requestWordListDto;
+		const { userId, sorting } = requestWordListDto;
+		const [sortOption, ascOrDesc] = WORD_SORTING_TYPE[sorting];
 
 		const queryBuilder = this.wordRepository.createQueryBuilder('word');
 
-		const [words, totalCount] = await Promise.all([
+		queryBuilder
+			.leftJoin('word.likes', 'like')
+			.select([
+				'word.id',
+				'word.name',
+				'word.description',
+				'word.diacritic',
+				'word.pronunciation',
+				'word.wrongPronunciations',
+				'word.exampleSentence',
+				'COUNT(like.id) AS likeCount',
+			]);
+
+		if (userId) {
 			queryBuilder
-				.leftJoinAndSelect(
-					'word.likes',
-					'like',
-					userId ? 'like.userId = :userId' : '',
-					{ userId },
-				)
-				.select([
-					'word.id',
-					'word.name',
-					'word.pronunciation',
-					'word.diacritic',
-					'word.description',
-					'word.createdAt',
-					'CASE WHEN like.id IS NOT NULL THEN true ELSE false END AS isLike',
+				.addSelect([
+					'SUM(CASE WHEN like.userId = :userId THEN 1 ELSE 0 END) > 0 AS isLike',
 				])
-				.orderBy('word.createdAt', 'ASC')
-				.offset(requestWordListDto.getSkip())
-				.limit(requestWordListDto.limit)
-				.getRawMany(),
-			queryBuilder.getCount(),
-		]);
+				.setParameters({ userId });
+		} else {
+			queryBuilder.addSelect(['false AS isLike']);
+		}
+
+		const words = await queryBuilder
+			.groupBy('word.id')
+			.orderBy(sortOption, ascOrDesc)
+			.offset(requestWordListDto.getSkip())
+			.limit(requestWordListDto.limit)
+			.getRawMany();
+
+		const totalCount = await this.wordRepository
+			.createQueryBuilder('word')
+			.getCount();
 
 		const responseWordListDto = plainToInstance(
 			ResponseWordListDto,
