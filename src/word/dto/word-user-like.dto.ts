26062f88a876cb55f27828c82c4c872f54d9715d
Baik Gwangin <gwangin1999@naver.com>
2024-05-28 22:28:46 +0900

    feat: 단어장 조회 Request DTO 에 sorting 옵션 추가

--- src/word/dto/word-user-like.dto.ts
+++ src/word/dto/word-user-like.dto.ts
@@ -1,13 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 import { Exclude } from 'class-transformer';
-import { IsArray, IsString, IsUUID } from 'class-validator';
+import { IsArray, IsIn, IsOptional, IsString, IsUUID } from 'class-validator';
 
 import { PaginationOptionDto } from '#/common/dto/pagination.dto';
+
+import {
+	SORTING_WORD_OPTION,
+	type SortingWordListOption,
+} from '../interface/word-list-sorting.interface';
 
 export class RequestWordUserLikeDto extends PaginationOptionDto {
 	@IsUUID()
 	userId: string;
+
+	@IsOptional()
+	@IsIn(SORTING_WORD_OPTION)
+	@ApiProperty({
+		type: 'enum',
+		enum: SORTING_WORD_OPTION,
+	})
+	sorting: SortingWordListOption = 'CREATED';
 }
 
 export class ResponseWordUserLikeDto {
