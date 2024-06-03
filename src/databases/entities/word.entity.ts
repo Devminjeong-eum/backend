3e2dcd77033f7f2ad80689cf9de87219a35d1694
RookieAND <gwangin1999@naver.com>
2024-06-03 23:37:54 +0900

    feat: 단어에 검색 용 Sequence field 추가

--- src/databases/entities/word.entity.ts
+++ src/databases/entities/word.entity.ts
@@ -16,49 +16,48 @@ import { Like } from './like.entity';
 export class Word {
 	@ApiProperty({
 		description: '단어 별 고유 UUID 입니다.',
-		type: String,
 	})
 	@PrimaryGeneratedColumn('uuid')
 	id: string;
 
 	@ApiProperty({
+		description: '단어 별로 부여되는 고유 Sequence 값입니다.',
+	})
+	@PrimaryGeneratedColumn()
+	sequence: number;
+
+	@ApiProperty({
 		description: '단어의 이름입니다.',
-		type: String,
 	})
 	@Column({ type: 'varchar', unique: true })
 	name: string;
 
 	@ApiProperty({
 		description: '단어에 대한 설명입니다.',
-		type: String,
 	})
 	@Column({ type: 'text' })
 	description: string;
 
 	@ApiProperty({
 		description: '단어의 발음 기호 목록입니다.',
-		type: Array<string>,
 	})
 	@Column({ type: 'varchar', array: true })
 	diacritic: string[];
 
 	@ApiProperty({
 		description: '단어의 올바른 발음 예시 목록입니다.',
-		type: Array<string>,
 	})
 	@Column({ type: 'varchar', array: true })
 	pronunciation: string[];
 
 	@ApiProperty({
 		description: '단어의 잘못된 발음 예시 목록입니다.',
-		type: Array<string>,
 	})
 	@Column({ type: 'varchar', array: true })
 	wrongPronunciations: string[];
 
 	@ApiProperty({
 		description: '단어 사용 예문입니다.',
-		type: String,
 	})
 	@Column({ type: 'text' })
 	exampleSentence: string;
