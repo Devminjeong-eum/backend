0a13fde82de4bfe1642a061430519c2efa7c22f3
RookieAND <gwangin1999@naver.com>
2024-06-08 00:06:11 +0900

    fix: Nginx 컨테이너에서 발생하는 502 에러 수정

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
 	const configService = app.get(ConfigService);
 	const port = configService.get<number>('SERVER_PORT') || 3000;
 
-	await app.listen(port);
+	await app.listen(port, '0.0.0.0');
 }
 bootstrap();
