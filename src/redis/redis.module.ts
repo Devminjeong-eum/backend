fb710be24ce1ca68c8a0ce4fdd1bbd19e296ff7a
RookieAND <gwangin1999@naver.com>
2024-06-10 00:50:16 +0900

    feat: RedisModule 에서 createClient - connect 메서드 체이닝 적용

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,34 +1,32 @@
-import {DynamicModule, Global, Module} from '@nestjs/common';
+import { DynamicModule, Global, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { createClient } from 'redis';
 
 export const REDIS_CLIENT = Symbol('REDIS_CLIENT');
 
 const redisConnectionFactory = async (configService: ConfigService) => {
-    const REDIS_PORT = configService.get<string>('REDIS_PORT');
-    const client = createClient({
-        url: `redis://localhost:${REDIS_PORT}`
-    })
-    await client.connect();
-    console.log(client);
-    return client;
-}
+	const REDIS_PORT = configService.get<string>('REDIS_PORT');
+	return await createClient({
+		url: `redis://localhost:${REDIS_PORT}`,
+	}).connect();
+};
 
 @Global()
 @Module({})
 export class RedisModule {
-    static forRootAsync(): DynamicModule {
-        return {
-            module: RedisModule,
-            imports: [ConfigModule],
-            providers: [
-                {
-                    provide: REDIS_CLIENT,
-                    inject: [ConfigService],
-                    useFactory: redisConnectionFactory
-                }
-            ],
-            exports: [REDIS_CLIENT]
-        }
-    }
+	static forRootAsync(): DynamicModule {
+		return {
+			module: RedisModule,
+			imports: [ConfigModule],
+			providers: [
+				{
+					provide: REDIS_CLIENT,
+					inject: [ConfigService],
+					useFactory: redisConnectionFactory,
+				},
+			],
+			exports: [REDIS_CLIENT],
+		};
+	}
 }
