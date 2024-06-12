27299f861007ed25454b42c8094981467d89d311
RookieAND <gwangin1999@naver.com>
2024-06-12 23:24:04 +0900

    feat: 퀴즈 선택지 생성 시 DTO 에서 배열 shuffle 하는 함수 추가

--- src/quiz/dto/quiz-selection.dto.ts
+++ src/quiz/dto/quiz-selection.dto.ts
@@ -3,6 +3,14 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Expose, Transform } from 'class-transformer';
 import { IsArray, IsString } from 'class-validator';
 
+const shuffle = (array: Array<unknown>) => {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j: number = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+	return array;
+};
+
 export class ResponseQuizSelectionDto {
 	@IsString()
 	@Transform(({ obj }) => obj.quizSelection_correct)
@@ -11,10 +19,12 @@ export class ResponseQuizSelectionDto {
 	correct: string;
 
 	@IsArray()
-	@Transform(({ obj }) => [
-		...obj.quizSelection_incorrectList,
-		obj.quizSelection_correct,
-	])
+	@Transform(({ obj }) =>
+		shuffle([
+			...obj.quizSelection_incorrectList,
+			obj.quizSelection_correct,
+		]),
+	)
 	@Expose({ name: 'selections' })
 	@ApiProperty()
 	selections: string[];
