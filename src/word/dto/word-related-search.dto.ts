ea5bf19c203b431e8fc8db15a98ccc33f55cef58
RookieAND <gwangin1999@naver.com>
2024-06-18 01:05:08 +0900

    hotfix: 연관 검색 단어의 경우에도 숫자 영어 조합을 허용하도록 수정

--- src/word/dto/word-related-search.dto.ts
+++ src/word/dto/word-related-search.dto.ts
@@ -1,13 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 import { Expose, Transform } from 'class-transformer';
-import { IsAlpha, IsString, IsUUID } from 'class-validator';
+import { IsString, IsUUID, Matches } from 'class-validator';
 
 import { PaginationOptionDto } from '#/common/dto/pagination.dto';
 
 export class RequestWordRelatedSearchDto extends PaginationOptionDto {
 	@IsString()
-	@IsAlpha()
+	@Matches(/^[a-zA-Z0-9]+$/)
 	@ApiProperty({
 		description: '입력한 단어 키워드',
 	})
