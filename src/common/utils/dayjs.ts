e33a4b800f14bac508871b4acc11810c807c0753
RookieAND <gwangin1999@naver.com>
2024-06-25 22:10:33 +0900

    feat: Dayjs 에 weekOfYear 플러그인 추가

--- src/common/utils/dayjs.ts
+++ src/common/utils/dayjs.ts
@@ -1,14 +1,16 @@
-import * as dayjs from 'dayjs';
+import dayjs from 'dayjs';
 import 'dayjs/locale/ko';
-import * as isBetween from 'dayjs/plugin/isBetween';
-import * as timezone from 'dayjs/plugin/timezone';
-import * as utc from 'dayjs/plugin/utc';
+import isBetween from 'dayjs/plugin/isBetween';
+import timezone from 'dayjs/plugin/timezone';
+import weekOfYear from 'dayjs/plugin/weekOfYear';
+import utc from 'dayjs/plugin/utc';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
+dayjs.extend(isBetween);
+dayjs.extend(weekOfYear);
 
 dayjs.locale('ko');
 dayjs.tz.setDefault('Asia/Seoul');
-dayjs.extend(isBetween);
 
 export default dayjs;
