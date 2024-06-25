f5b70a1d1efeb3889f8bd4dc2aa984313ab6ad6b
RookieAND <gwangin1999@naver.com>
2024-06-25 22:43:43 +0900

    feat: 주차 별, 월 별, 연도 별 랭킹 정보를 받아오는 Service 로직 구현

--- src/ranking/ranking.service.ts
+++ src/ranking/ranking.service.ts
@@ -1,12 +1,32 @@
 import { Injectable } from '@nestjs/common';
 
 import { RankingRepository } from '#/databases/repositories/ranking.repository';
+import { RequestRankingByWeekDto } from './dto/rank-by-week.dto';
+import { RequestRankingByMonthDto } from './dto/rank-by-month.dto';
+import { RequestRankingByYearDto } from './dto/rank-by-year.dto';
 
 @Injectable()
 export class RankingService {
 	constructor(private readonly rankingRepository: RankingRepository) {}
 
-	findCurrentRanking() {
-		return `This action returns all ranking`;
+	findCurrentWeekRanking() {
+		const ranking = this.rankingRepository.findByCurrentWeek();
+		return ranking;
+	}
+
+
+	findSpecificWeekRanking(rankingByWeekDto: RequestRankingByWeekDto) {
+		const ranking = this.rankingRepository.findBySpecificWeek(rankingByWeekDto);
+		return ranking;
+	}
+
+	findSpecificMonthRanking(rankingByMonthDto: RequestRankingByMonthDto) {
+		const ranking = this.rankingRepository.findBySpecificMonth(rankingByMonthDto);
+		return ranking;
+	}
+
+	findSpecificYearRanking(rankingByYearDto: RequestRankingByYearDto) {
+		const ranking = this.rankingRepository.findBySpecificYear(rankingByYearDto);
+		return ranking;
 	}
 }
