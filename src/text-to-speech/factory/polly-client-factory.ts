1e1926f7a1552a3b4ac696cdfb9d0611e0229c8b
RookieAND <gwangin1999@naver.com>
2024-07-14 23:27:05 +0900

    feat: 환경 변수가 존재하지 않을 경우에 대한 에러 정의

--- src/text-to-speech/factory/polly-client-factory.ts
+++ src/text-to-speech/factory/polly-client-factory.ts
@@ -1,8 +1,27 @@
-import { PollyClient } from "@aws-sdk/client-polly";
+import { ConfigService } from '@nestjs/config';
 
-const REGION = "ap-northeast-2";
+import { PollyClient } from '@aws-sdk/client-polly';
 
-export const createAwsPollyClientFactory = () => {
-    const pollyClient = new PollyClient({ region: REGION });
-    return pollyClient;
-}
+import { RequireEnvironmentVariableException } from '#/common/exceptions/RequireEnvironmentVariableException';
+
+const REGION = 'ap-northeast-2';
+
+export const createAwsPollyClientFactory = (configService: ConfigService) => {
+	const iamAccessKey = configService.get<string>('AWS_IAM_ACCESS_KEY');
+	const iamSecretAccessKey = configService.get<string>(
+		'AWS_IAM_SECRET_ACCESS_KEY',
+	);
+
+	if (!iamAccessKey || !iamSecretAccessKey) {
+		const notExistsVariables = [
+			iamAccessKey ? undefined : 'AWS_IAM_ACCESS_KEY',
+			iamSecretAccessKey ? undefined : 'AWS_IAM_SECRET_ACCESS_KEY',
+		]
+			.filter(Boolean)
+			.join(' ,');
+		throw new RequireEnvironmentVariableException(notExistsVariables);
+	}
+
+	const pollyClient = new PollyClient({ region: REGION });
+	return pollyClient;
+};
