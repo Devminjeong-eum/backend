d3f0237a6d063c72bbd2ff0063fe697384deca39
RookieAND <gwangin1999@naver.com>
2024-07-15 00:42:36 +0900

    feat: TTS Module 에 S3Client 에 대한 Provider 추가

--- src/text-to-speech/text-to-speech.module.ts
+++ src/text-to-speech/text-to-speech.module.ts
@@ -1,10 +1,13 @@
 import { Module, Provider } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
-import { AWS_POLLY_CLIENT } from './constant';
-import { createAwsPollyClientFactory } from './factory/polly-client-factory';
-import { TextToSpeechService } from './text-to-speech.service';
 import { PollyClient } from '@aws-sdk/client-polly';
+import { S3Client } from '@aws-sdk/client-s3';
+
+import { AWS_POLLY_CLIENT, AWS_S3_BUCKET } from './constant';
+import { createAwsPollyClientFactory } from './factory/polly-client-factory';
+import { createAwsS3BucketFactory } from './factory/s3-bucket-factory';
+import { TextToSpeechService } from './text-to-speech.service';
 
 const AwsPollyProvider: Provider<PollyClient> = {
 	provide: AWS_POLLY_CLIENT,
@@ -12,8 +15,14 @@ const AwsPollyProvider: Provider<PollyClient> = {
 	inject: [ConfigService],
 };
 
+const AwsS3BucketProvider: Provider<S3Client> = {
+	provide: AWS_S3_BUCKET,
+	useFactory: createAwsS3BucketFactory,
+	inject: [ConfigService],
+};
+
 @Module({
-	providers: [TextToSpeechService, AwsPollyProvider],
-	exports: [TextToSpeechService]
+	providers: [TextToSpeechService, AwsPollyProvider, AwsS3BucketProvider],
+	exports: [TextToSpeechService],
 })
 export class TextToSpeechModule {}
