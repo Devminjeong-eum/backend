1b35e2dd82ac4f8abe7614bd6338d1b25e4249d5
RookieAND <gwangin1999@naver.com>
2025-01-18 20:51:47 +0900

    fix: development 모드일 경우 logger 설정을 true 로 전환하도록 수정

--- src/infrastructure/drizzle/factory/drizzle-client.factory.ts
+++ src/infrastructure/drizzle/factory/drizzle-client.factory.ts
@@ -1,21 +1,24 @@
 import type { ConfigService } from '@nestjs/config';
 
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { type NodePgDatabase, drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 
 import type { DrizzleModuleForRootOption } from '../interface/drizzle-module-option.interface';
 import * as schema from '../schema';
 
-export const createDrizzleClient = async (
+export const createDrizzleClient = (
 	configService: ConfigService,
 	option?: DrizzleModuleForRootOption,
-) => {
+): NodePgDatabase<typeof schema> => {
+	const isDevelopment =
+		configService.getOrThrow('NODE_ENV') === 'development';
+
 	const pool = new Pool({
 		user: configService.getOrThrow('DB_USERNAME'),
 		host: configService.getOrThrow('DB_HOST'),
 		database: configService.getOrThrow('DB_DATABASE'),
 		password: configService.getOrThrow('DB_PASSWORD'),
-		port: parseInt(configService.getOrThrow('DB_PORT')),
+		port: parseInt(configService.getOrThrow<'number'>('DB_PORT')),
 		// TODO : 인증서를 적용하여 SSL 연결이 가능하도록 수정 필요
 		ssl: {
 			rejectUnauthorized: false,
@@ -23,5 +26,10 @@ export const createDrizzleClient = async (
 		...option?.pool,
 	});
 
-	return drizzle({ ...option?.drizzle, client: pool, schema: { ...schema } });
+	return drizzle({
+		...option?.drizzle,
+		client: pool,
+		logger: isDevelopment,
+		schema: { ...schema },
+	});
 };
