01aec56a3669c0ee5db47d6918031e3d11c7309f
RookieAND <gwangin1999@naver.com>
2025-03-03 02:09:03 +0900

    feat: User Schema 에 UserRole Enum 타입을 가지는 role Column 추가

--- src/infrastructure/drizzle/schema/user.schema.ts
+++ src/infrastructure/drizzle/schema/user.schema.ts
@@ -2,18 +2,22 @@ import { ApiProperty } from '@nestjs/swagger';
 
 import {
 	IsDate,
+	IsEnum,
 	IsNotEmpty,
 	IsOptional,
 	IsString,
 	IsUrl,
 } from 'class-validator';
 import { relations } from 'drizzle-orm';
-import { pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { pgEnum, pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
 
 import { timestamps } from '../helper/timestamp.helper';
 
 import { like } from './like.schema';
 import { Exclude } from 'class-transformer';
+import { UserRole } from '../constant/user-role.constant';
+
+const roleEnum = pgEnum('user_role', [UserRole.ADMIN, UserRole.USER])
 
 export const user = pgTable('user', {
 	id: varchar().notNull().primaryKey(),
@@ -21,6 +25,7 @@ export const user = pgTable('user', {
 	profileImage: varchar().notNull(),
 	socialType: varchar().notNull(),
 	socialPlatformId: varchar().notNull(),
+	role: roleEnum().default(UserRole.USER),
 	deletedAt: timestamp({ mode: 'date' }),
 	...timestamps,
 });
@@ -61,6 +66,10 @@ export class UserSchema implements UserEntity {
 	@IsNotEmpty()
 	@ApiProperty({ type: String, required: true, example: '123456789' })
 	socialPlatformId: string;
+
+	@IsEnum(Object.values(UserRole))
+	@ApiProperty({ enum: UserRole })
+	role: UserRole;
 
 	@IsDate()
 	@IsOptional()
