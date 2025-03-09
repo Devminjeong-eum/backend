7ead7afd208e675e9c7106d85cda9d3548b83103
RookieAND <gwangin1999@naver.com>
2025-03-09 23:25:41 +0900

    feat: RedisClient 타입 생성 및 Factory Return Type 에 이를 적용

--- src/infrastructure/redis/factory/redis-client.factory.ts
+++ src/infrastructure/redis/factory/redis-client.factory.ts
@@ -2,9 +2,11 @@ import type { ConfigService } from '@nestjs/config';
 
 import { createClient } from 'redis';
 
+import type { RedisClient } from '../interface/redis-client.interface';
+
 export const redisConnectionFactory = async (
 	configService: ConfigService,
-): Promise<ReturnType<typeof createClient>> => {
+): Promise<RedisClient> => {
 	const REDIS_HOST = configService.getOrThrow<string>('REDIS_HOST');
 	const REDIS_PORT = configService.getOrThrow<string>('REDIS_PORT');
 	return await createClient({
